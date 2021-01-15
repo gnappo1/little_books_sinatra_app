26d29afbc00070390b73bee519777b55aeec4426
Matteo <gnappo.1@live.it>
2021-01-14 18:14:52 -0800

    Add README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# Books Sinatra Application with Active Record ORM
 
+- We create a quick scaffolding for the project with the `corneal` gem by running: `corneal new little_books_app`.
+- Then start working on the database: complete the migration files and run the migrations.
+- Start working on the models and use macros to build associations.
+- Add the appropriate controllers and view files
+- run `shotgun` to start the server (always quit with cntrl + c)
--- db/migrate/20210114234712_create_books.rb
+++ db/migrate/20210114234712_create_books.rb
@@ -9,3 +9,33 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     end
   end
 end
+
+@patient = Patient.new(name: “Matteo”)
+	if @patient.save #if it correctly persists the instance
+		#success message with flash
+		redirect ‘/patients’ #where in the list of patients we now see the new patient
+	else #if validations reject the persistence attempt
+		#create an error message possibly with flash
+		redirect ‘/new’ #where the form to create a new patient lives
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
